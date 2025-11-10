1df9fcd5b6a8dfc542730be94edcef396b2b33c8
Colton Paul <ColtonPaul@users.noreply.github.com>
2025-11-09 21:14:37 -0800

    Adjust fabricmanager download path for version >=580

--- components/install_nvidia_fabric_manager.sh
+++ components/install_nvidia_fabric_manager.sh
@@ -9,13 +9,19 @@ nvidia_fabricmanager_metadata=$(jq -r '.fabricmanager' <<< $nvidia_metadata)
 NVIDIA_FABRICMANAGER_DISTRIBUTION=$(jq -r '.distribution' <<< $nvidia_fabricmanager_metadata)
 NVIDIA_FABRICMANAGER_VERSION=$(jq -r '.version' <<< $nvidia_fabricmanager_metadata)
 NVIDIA_FABRICMANAGER_SHA256=$(jq -r '.sha256' <<< $nvidia_fabricmanager_metadata)
+NVIDIA_FABRICMANAGER_PREFIX=$(echo $NVIDIA_FABRICMANAGER_VERSION | cut -d '.' -f1)
 
 if [[ $DISTRIBUTION == *"ubuntu"* ]]; then
-    NVIDIA_FABRICMANAGER_PREFIX=$(echo $NVIDIA_FABRICMANAGER_VERSION | cut -d '.' -f1)
-    NVIDIA_FABRIC_MNGR_PKG=http://developer.download.nvidia.com/compute/cuda/repos/${NVIDIA_FABRICMANAGER_DISTRIBUTION}/x86_64/nvidia-fabricmanager-${NVIDIA_FABRICMANAGER_PREFIX}_${NVIDIA_FABRICMANAGER_VERSION}_amd64.deb
+    # Use different URL format for NVIDIA Fabric Manager major version 580 and above
+    if [[ $NVIDIA_FABRICMANAGER_PREFIX -ge 580 ]]; then
+        NVIDIA_FABRIC_MNGR_PKG=http://developer.download.nvidia.com/compute/cuda/repos/${NVIDIA_FABRICMANAGER_DISTRIBUTION}/x86_64/nvidia-fabricmanager_${NVIDIA_FABRICMANAGER_VERSION}_amd64.deb
+    else
+        NVIDIA_FABRIC_MNGR_PKG=http://developer.download.nvidia.com/compute/cuda/repos/${NVIDIA_FABRICMANAGER_DISTRIBUTION}/x86_64/nvidia-fabricmanager-${NVIDIA_FABRICMANAGER_PREFIX}_${NVIDIA_FABRICMANAGER_VERSION}_amd64.deb
+    fi
+
     FILENAME=$(basename $NVIDIA_FABRIC_MNGR_PKG)
     download_and_verify ${NVIDIA_FABRIC_MNGR_PKG} ${NVIDIA_FABRICMANAGER_SHA256}
-    
+
     apt install -y ./${FILENAME}
     apt-mark hold nvidia-fabricmanager-${NVIDIA_FABRICMANAGER_PREFIX}
 elif [[ $DISTRIBUTION == almalinux* ]]; then    
